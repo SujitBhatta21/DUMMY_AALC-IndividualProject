3eb17375bd5d4e741ab71b3ff83ecd377dfff456
SujitBhatta21 <bhattasujit9@gmail.com>
2026-01-31 12:33:22 +0000

    Bonus Feature: Added tutorial guide in Timeline page.
    Used driver.js which was really lightweight and easy to implement. Also, it falls under MIT Lisence so, there is no lisencing issues.

--- client/src/pages/TimelinePage.tsx
+++ client/src/pages/TimelinePage.tsx
@@ -1,9 +1,70 @@
 import '../styles/TimelinePage.css'
-import {Header} from "../components/Header.tsx";
+import { Header } from "../components/Header.tsx";
 import {Link} from "react-router-dom";
+import { useEffect } from "react";
+import { driver } from 'driver.js';
+import 'driver.js/dist/driver.css';
 
 
 function TimelinePage() {
+    const DEVELOPMENT_MODE = true; // I set this up so that I can test popup tour.
+
+    useEffect(() => {
+        const hasSeenTutorial = localStorage.getItem('hasSeenTimelineTutorial');
+
+        if (DEVELOPMENT_MODE || !hasSeenTutorial) {
+            const driverObj = driver({
+                showProgress: true,
+                steps: [
+                    {
+                        popover: {
+                            title: 'Welcome to Interactive Memory Shards!',
+                            description: 'Solve all puzzles in order to collect shards and uncover history.',
+                        }
+                    },
+                    {
+                        element: '.timeline-track1',
+                        popover: {
+                            title: 'Track 1',
+                            description: 'Learn how apartheid began in South Africa and some out of many key events',
+                        }
+                    },
+                    {
+                        element: '.timeline-track2',
+                        popover: {
+                            title: 'Track 2',
+                            description: 'Discover UK activism & connection with 28 Penton Street',
+                        }
+                    },
+                    {
+                        element: '.timeline-track3',
+                        popover: {
+                            title: 'Track 3',
+                            description: 'Explore how people worldwide supported the fight against apartheid through boycotts, music, and solidarity.',
+                        }
+                    },
+                    {
+                        element: '.shards-collected-btn',
+                        popover: {
+                            title: 'Collect Shards',
+                            description: 'Collect all 9 shards to reveal the final message!',
+                        }
+                    },
+                ],
+                onDestroyed: () => {
+                    if (!DEVELOPMENT_MODE) {
+                        localStorage.setItem('hasSeenTimelineTutorial', 'true');
+                    }
+                }
+            });
+
+            setTimeout(() => {
+                driverObj.drive();
+            }, 500);
+        }
+    }, []);
+
+
     return (
         <div className="timeline-page">
             <Header/>
