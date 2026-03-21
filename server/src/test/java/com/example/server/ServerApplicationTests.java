4e9e5fd359dc2717528c55039a465d87f66420cf
SujitBhatta21 <bhattasujit9@gmail.com>
2026-03-21 11:43:09 +0000

    Feature: Running ServerApplicationTests runs all test files.
    But needed correct configuration prior to running.

--- server/src/test/java/com/example/server/ServerApplicationTests.java
+++ server/src/test/java/com/example/server/ServerApplicationTests.java
@@ -1,8 +1,14 @@
 package com.example.server;
 
-import org.junit.jupiter.api.Test;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
+@Suite
+@SelectPackages("com.example.server")
 class ServerApplicationTests {
+    // Running this file with correct environment variable configuration
+    // automatically calls all test files inside controller, repository and service directory.
 }
